fc77d49ae67f9df37ab388854dd68f495dd87d0f
ophilon <ophil44@gmail.com>
2024-09-25 09:30:09 +0300

    server.js add some debug info;upd. package-lock.json

--- web/server.js
+++ web/server.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const os = require('os');
 const express = require('express');
 const app = express();
@@ -13,12 +15,31 @@ app.get('/', function(req, res) {
         if (isNaN(numVisitsToDisplay)) {
             numVisitsToDisplay = 1;
         }
-       res.send(os.hostname() +': Number of visits is: ' + numVisitsToDisplay);
+       res.send(os.hostname() +': Number of visits is: ' + numVisitsToDisplay + '\n');
         numVisits++;
         redisClient.set('numVisits', numVisits);
     });
 });
 
+const dirs = fs.readdirSync('node_modules');
+const data = {};
+//add ones you care about
+const trackedPackages = ['express', 'passport', 'body-parser'];
+dirs.forEach(function(dir) {
+
+   if(trackedPackages.indexOf(dir) > -1){
+      try{
+        const json = JSON.parse(
+          fs.readFileSync(path.join('node_modules', dir, 'package.json'), 'utf8')
+        );
+        data[dir] = json.version;
+      }catch(e){
+        console.log(`failed to read/parse package.json for ${dir}`, e);
+      }
+   }
+
+});
+console.debug(data['express']); //= 4.11.2
 app.listen(5000, function() {
     console.log('Web application is listening on port 5000');
 });
